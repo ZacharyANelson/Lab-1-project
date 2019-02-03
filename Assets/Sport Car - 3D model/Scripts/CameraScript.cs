40b9b4363ea5a12e4e82851d39c429e1756cf493
Blake <blake.hawkins97@gmail.com>
2019-02-03 00:21:57 -0600

    Updated camera controller
    Camera now follows the car model when it moves

--- Assets/Sport Car - 3D model/Scripts/CameraScript.cs
+++ Assets/Sport Car - 3D model/Scripts/CameraScript.cs
@@ -7,9 +7,13 @@ public class CameraScript : MonoBehaviour {
     private bool MouseClick = false;
     public float cameraRotateSpeed = 5f;
     public Transform RotateTarget;
+    public Transform transformObject; // Position of the object (The car in this case)
+    private Vector3 distance;
+    private float smooth = 0.5f;
 
-	void Start () {
-	}
+    void Start () {
+        distance = transform.position - transformObject.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,5 +22,7 @@ public class CameraScript : MonoBehaviour {
         if (Input.GetMouseButtonUp(0)) MouseClick = false;
 
         if(MouseClick) transform.RotateAround(RotateTarget.position, RotateTarget.up, Input.GetAxis("Mouse X") * cameraRotateSpeed);
+        Vector3 newPosition = transformObject.position + distance;
+        transform.position = Vector3.Slerp(transform.position, newPosition, smooth);
     }
 }
